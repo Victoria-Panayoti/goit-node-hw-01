bf367a27b09e9c541c77251b8a80fea534c30ed0
Victoria Panajoti <112144156+Victoria-Panayoti@users.noreply.github.com>
2023-06-25 09:04:42 +0300

    made homework1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # goit-node-hw-01
+
+link to screenshots
+https://monosnap.com/list/6497d58e18c457269f49517a
---
+++ index.js
@@ -0,0 +1,40 @@
+const { program } = require("commander");
+const contacts = require("./contacts");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      console.log(allContacts);
+      break;
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      console.log(oneContact);
+      break;
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      console.log(newContact);
+      break;
+    case "remove":
+      const removedContact = await contacts.removeContact(id);
+      console.log(removedContact);
+      break;
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+program
+  .option("-a, --action <action>", "Action to invoke")
+  .option("-i, --id <id>", "Contact id")
+  .option("-n, --name <name>", "Contact name")
+  .option("-e, --email <email>", "Contact email")
+  .option("-p, --phone <phone>", "Contact phone");
+
+program.parse(process.argv);
+const options = program.opts();
+invokeAction(options);
+
+// invokeAction({ action: 'list' });
+// invokeAction({action:'get',id:'rsKkOQUi80UsgVPCcLZZW'})
+// invokeAction({action:'add', name:'Marsel',email:'marsel@mail.com', phone:'0678546758'})
+// invokeAction({ action: 'remove', id: 'rsKkOQUi80UsgVPCcLZZW' });
